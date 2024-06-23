eb5ed03d93cf22fd61374615987307676e779551
Mohit Lahoti <engtechno25@gmail.com>
2024-06-24 00:41:46 +0530

    Creating dummy template sending and sender's message sending functionality

--- whatsappChat/urls.py
+++ whatsappChat/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('indContact', views.indPhone.as_view()),
-    path('sendMsg', views.sendMsg.as_view()),
+    path('template-send', views.sendTemplate.as_view()),
+    path('message-send', views.sendMessage.as_view()),
     path('excelfile', views.csvPhone.as_view()),
 ]
--- whatsappChat/views.py
+++ whatsappChat/views.py
@@ -3,9 +3,8 @@ from rest_framework.response import Response
 from . import renderers
 from rest_framework import status
 from . import serializers
-import requests
+import requests, json
 
-# Create your views here.
 class indPhone(APIView):
     renderer_classes = [renderers.MessageRenderers]
     def post(self, request, format=None):
@@ -24,26 +23,58 @@ class csvPhone(APIView):
             serializer.save();
             return Response({'msg': "Data Saved"}, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    
-class sendMsg(APIView):
+
+class sendTemplate(APIView):
     def post(self, request):
         mobile_number = request.data['phone']
-        url = "https://graph.facebook.com/v19.0/293632593827326/messages"
-        headers = {
-            "Authorization": "Bearer EAAL7og371v4BOZBtKG2MiPiI6eSxtt06tC69MjxUxkWiwX1o2bs4Qh7ZAv4pLJQxMJHm5txyggAArBL1pMNm2GeGMlyAeQYeP3qC63O6bZBFHdal3uBsTTdJhZClB3lu4sQdjet0QkU8ksU1thNBmEW36eO1vzk6wmkQ7GbxaAZAFTYwQ1cduW9fMHqveBGfio2ELGNru08gU2ajckgcZD",
-            "Content-Type": "application/json"
-        }
-        payload = {
-            "messaging_product": "whatsapp",
-            "to": "91"+mobile_number,
-            "type": "template",
-            "template": {
-                "name": "hello_world",
-                "language": {
-                    "code": "en_US"
+        AUTH_TOKEN = "EAAL7og371v4BOzH3qCP6o1o2WdIfdwZBU0M2Oy9xhqSK9IwJ1jQJjs1P69z3Kq9dchPdpuGc0MpL4Wna2XMxHFXivD4qPfDS1zZA7H7SY60H8IzmGh4wTFQ4H4Uz5SZAlCmYDiLZCRK3vDaRrUomNpiKFH6KfD2qTcnAceRRIZCFiUz3GOgVZBoMHl0ZAOXvz7mgAZDZD"
+        try:
+            url = "https://graph.facebook.com/v19.0/275772712289918/messages"
+            headers = {
+                "Authorization": f"Bearer {AUTH_TOKEN}",
+                "Content-Type": "application/json"
+            }
+            data = {
+                "messaging_product": "whatsapp",
+                "to": mobile_number,
+                "type": "template",
+                "template": {
+                    "name": "hello_world",
+                    "language": {
+                        "code": "en_US"
+                    }
                 }
             }
-        }
-        response = requests.post(url, headers=headers, json=payload)
-        return Response({'msg': response}, status=status.HTTP_200_OK)
+            response = requests.post(url, headers=headers, json=data)
+            print(response.json())
+            return Response({'msg': response}, status=status.HTTP_200_OK)
+        except:
+            return Response({"msg": "Error comes..."}, status=status.HTTP_400_BAD_REQUEST)
+
+class sendMessage(APIView):
+    def post(self, request):
+        mobile_number = request.data['phone']
+        message = request.data['message']
+        url = "https://graph.facebook.com/v19.0/275772712289918/messages"
+        ACCESS_TOKEN = "EAAL7og371v4BOzH3qCP6o1o2WdIfdwZBU0M2Oy9xhqSK9IwJ1jQJjs1P69z3Kq9dchPdpuGc0MpL4Wna2XMxHFXivD4qPfDS1zZA7H7SY60H8IzmGh4wTFQ4H4Uz5SZAlCmYDiLZCRK3vDaRrUomNpiKFH6KfD2qTcnAceRRIZCFiUz3GOgVZBoMHl0ZAOXvz7mgAZDZD"
+        try:
+            payload = json.dumps({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": mobile_number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": message
+            }
+            })
+            headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {ACCESS_TOKEN}'
+            }
+            response = requests.request("POST", url, headers=headers, data=payload)
+            print(response.text)
+            return Response({'msg': response}, status=status.HTTP_200_OK)
+        except:
+            return Response({"msg": "Error comes..."}, status=status.HTTP_400_BAD_REQUEST)
+        
